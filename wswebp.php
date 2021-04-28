42d31dfff99445b2e9a68e3abc429b3c7b49103a
Sergey Bolotov <sales@webstudio.pro>
2021-04-28 18:48:54 +0300

    ver 1.0.1
    fixed png bug + k2 working

--- wswebp.php
+++ wswebp.php
@@ -95,6 +95,7 @@ class PlgContentWswebp extends JPlugin
     }
     public static function createWebp($file,$extension,$quality)
     {
+
         $file1=JPATH_SITE.$file;
         if (in_array($extension,['jpeg','jpg'])) $image=imagecreatefromjpeg($file1);
         if ($extension=='png') $image=imagecreatefrompng($file1);
@@ -102,23 +103,25 @@ class PlgContentWswebp extends JPlugin
         if ($extension=='gif') $image=imagecreatefromgif($file1);
         ob_start();
         if (in_array($extension,['jpeg','jpg'])) imagejpeg($image,NULL,100);
-        if ($extension=='png') imagepng($image,NULL,100);
+        if ($extension=='png') imagepng($image,NULL,9);
         if ($extension=='bmp') imagebmp($image,NULL,100);
         if ($extension=='gif') imagegif($image,NULL,100);
         $cont= ob_get_contents();
         ob_end_clean();
-        imagedestroy($image1);
+        imagedestroy($image);
         $content =  imagecreatefromstring($cont);
         imagewebp($content,$file1.".webp",$quality);
         imagedestroy($content);
+
     }
 
-    public function onContentPrepare($context, $item, $params, $limitstart = 0)
+    public function onContentPrepare($context, &$item, $params, $limitstart = 0)
     {
-        // var_dump($context); // mod_custom.content com_content.article
+         // var_dump($context); // mod_custom.content com_content.article
         //$b=get_browser(null,true);
         //$b=$_SERVER['HTTP_ACCEPT'];
         // var_dump($b);
+
 
         if( strpos( $_SERVER['HTTP_ACCEPT'], 'image/webp' ) === false ) {
             // webp is supported!
@@ -137,7 +140,40 @@ class PlgContentWswebp extends JPlugin
             if ($modules_except==0 && is_array($e_modules) && (!in_array($module,$e_modules))) return;
         }
 
-        $item->text=ImageUrlReplacer::replace($item->text);
+        // var_dump($item);
+        $text=&$item->text;
+        if (in_array($context,['com_k2.itemlist']))
+        {
+            $to_replace=['imageXSmall','imageSmall','imageMedium',
+                'imageLarge','imageXLarge','imageGeneric'];
+
+            foreach ($to_replace as $k=>$t)
+            {
+                if (!isset($item->$t)) continue;
+                $ext=strtolower(pathinfo($item->$t)['extension']);
+                if (in_array($ext,['jpg','jpeg','png','gif','bmp']))
+                    {
+
+                        $replacing=$item->$t;
+                        $item->$t=$item->$t.'.webp';
+                        if (mb_substr($replacing,0,1)!="/") $replacing="/".$replacing;
+                        if (file_exists(JPATH_SITE.$replacing.".webp")) continue; // уже есть - отличненько
+                        if ($this->params->get('create_webp')==1) self::createWebp($replacing,$ext,$this->params->get('webp_quality',80));
+
+                    }
+
+            }
+            // var_dump($t);
+            if ($this->params->get('debug')==1)var_dump($item->$t);
+        }
+//        public 'imageXSmall' => string '/media/k2/items/cache/fbe0cb468ebf47784fd8b26af4021e7e_XS.jpg' (length=61)
+//  public 'imageSmall' => string '/media/k2/items/cache/fbe0cb468ebf47784fd8b26af4021e7e_S.jpg' (length=60)
+//  public 'imageMedium' => string '/media/k2/items/cache/fbe0cb468ebf47784fd8b26af4021e7e_M.jpg' (length=60)
+//  public 'imageLarge' => string '/media/k2/items/cache/fbe0cb468ebf47784fd8b26af4021e7e_L.jpg' (length=60)
+//  public 'imageXLarge' => string '/media/k2/items/cache/fbe0cb468ebf47784fd8b26af4021e7e_XL.jpg' (length=61)
+//  public 'imageGeneric'
+
+        $text=ImageUrlReplacer::replace($text);
 
         if ($this->params->get('debug')==1)  var_dump(ImageUrlReplacer::$replaced);
         if ($this->params->get('create_webp')==1)
@@ -145,10 +181,11 @@ class PlgContentWswebp extends JPlugin
             // проверяем файлики и создаем если нет
             foreach (ImageUrlReplacer::$replaced as &$replacing)
             {
-                //var_dump($replacing);
+                // var_dump($replacing);
                 //var_dump(mb_substr($replacing,0,1));
                 if (mb_substr($replacing,0,1)!="/") $replacing="/".$replacing;
-                //var_dump($replacing);
+                if ($this->params->get('debug')==1) var_dump($replacing);
+                var_dump(file_exists(JPATH_SITE.$replacing.".webp"));
                 if (file_exists(JPATH_SITE.$replacing.".webp")) continue; // уже есть - отличненько
                 $ext=strtolower(pathinfo($replacing)['extension']);
                 if (in_array($ext,['jpg','jpeg','png','gif','bmp'])) self::createWebp($replacing,$ext,$this->params->get('webp_quality',80));
